f2c0883112b8523d0a81f064ffe3be92ae5a88d0
student <student@APC02HM0Z4DV7N.local>
2014-11-19 14:36:04 -0500

    Initial Commit

---
+++ Project/BuildingInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuildingInfoViewController.h
+//  Project
+//
+//  Created by student on 11/19/14.
+//  Copyright (c) 2014 cs@eku.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuildingInfoViewController : UIViewController
+
+@end
